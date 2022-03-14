0ee07142707d668bc0e43b4a0c7c8575d80f10f8
harvardhube <101551231+harvardhube@users.noreply.github.com>
2022-03-14 11:29:29 +0330

    good

---
+++ index.js
@@ -0,0 +1,11 @@
+var audio = new Audio('gta1.mp3');
+document.getElementById("play").addEventListener("click", function (){
+    document.getElementById("result").innerText = "🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣"
+})
+document.getElementById("po").addEventListener("click", function () {
+    audio.pause();
+})
+document.getElementById("submit").addEventListener("click", function (){
+    document.getElementById("result").innerText = "wrong captcha";
+})
+
